0ae1c6e501bffe820c2d24a5e032ec8e04a1a17f
Shahriar Zaman <sz.nayeem@topofstacksoftware.com>
2023-11-14 19:23:13 +0600

    - unzip updated

--- toolbox/src/main/java/media/uqab/toolbox/ZipExt.kt
+++ toolbox/src/main/java/media/uqab/toolbox/ZipExt.kt
@@ -1,10 +1,32 @@
-package media.uqab.toolbox
+/*
+MIT License
+
+Copyright (c) [2023] [Shahriar Zaman]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package com.quran_library.utils.fuzzy_search
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.documentfile.provider.DocumentFile
 import java.io.File
-import java.io.FileNotFoundException
 import java.util.zip.ZipFile
 
 /**
@@ -19,8 +41,7 @@ import java.util.zip.ZipFile
  * @return the extracted directory i.e, [extractTo] folder if [extractHere] is `true`
  * and [extractTo]\zipFile\ folder otherwise.
  */
-@RequiresApi(Build.VERSION_CODES.KITKAT)
-private fun extractZipFile(
+fun extractZipFile(
     zipFile: File,
     extractTo: File,
     extractHere: Boolean = false,
@@ -32,23 +53,23 @@ private fun extractZipFile(
             File(extractTo, zipFile.nameWithoutExtension)
         }
 
-        ZipFile(zipFile).use { zip ->
-            zip.entries().asSequence().forEach { entry ->
-                zip.getInputStream(entry).use { input ->
-                    if (entry.isDirectory) {
-                        val d = File(outputDir, entry.name)
-                        if (!d.exists()) d.mkdirs()
-                    } else {
-                        val f = File(outputDir, entry.name)
-                        if (f.parentFile?.exists() != true)  f.parentFile?.mkdirs()
+        val zip = ZipFile(zipFile)
+        zip.entries().asSequence().forEach { entry ->
+            zip.getInputStream(entry).use { input ->
+                if (entry.isDirectory) {
+                    val d = File(outputDir, entry.name)
+                    if (!d.exists()) d.mkdirs()
+                } else {
+                    val f = File(outputDir, entry.name)
+                    if (f.parentFile?.exists() != true)  f.parentFile?.mkdirs()
 
-                        f.outputStream().use { output ->
-                            input.copyTo(output)
-                        }
+                    f.outputStream().use { output ->
+                        input.copyTo(output)
                     }
                 }
             }
         }
+        zip.close()
 
         extractTo
     } catch (e: Exception) {
