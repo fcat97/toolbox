7f486a71f758126e329fb8842f5f61d44bc4f376
Shahriar Zaman <nayeem.zxc@gmail.com>
2025-09-25 15:02:40 +0600

    Update git-utils-installer.sh

--- scripts/git-utils-installer.sh
+++ scripts/git-utils-installer.sh
@@ -10,7 +10,8 @@ RED='\033[0;31m'
 RESET='\033[0m'
 
 TOOLBOX_DIR="$HOME/.fcat97/toolbox/scripts"
-SCRIPT_URL="https://raw.githubusercontent.com/fcat97/toolbox/refs/heads/master/scripts/git-utils.sh"
+# copy parmalink => open in incognito => click on raw button => copy url
+SCRIPT_URL="https://raw.githubusercontent.com/fcat97/toolbox/724f9ce36ef2ca032f497176351aae9e8944807a/scripts/git-utils.sh"
 SCRIPT_NAME="git-utils.sh"
 TARGET_SCRIPT="$TOOLBOX_DIR/$SCRIPT_NAME"
 
